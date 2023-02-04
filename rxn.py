66cae8841df0b64bbb264c6a22069558b5a75815
ARYAN CHAUDHARY <97864623+XERX-XD@users.noreply.github.com>
2023-02-04 18:33:58 +0545

    Update rxn.py

--- rxn.py
+++ rxn.py
@@ -1,12 +1,13 @@
 import os
 import platform
 print("JOIN FB GROUP")
+os.system('clear')
 os.system("xdg-open https://facebook.com/groups/460389902592352/")
 os.system("git pull")
 b = platform.architecture()[0]
 if b == '64bit':
-    import RXN64.cpython-311.so
+    import RXN64
     
 elif b == '32bit':
-    import rxn32.cpython-311.so
+    import rxn32
     
