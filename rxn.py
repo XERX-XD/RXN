f8c88ee3a0b994cc5bdb79b325e2758c8a2c46ff
ARYAN CHAUDHARY <97864623+XERX-XD@users.noreply.github.com>
2023-02-05 17:22:44 +0545

    Update rxn.py

--- rxn.py
+++ rxn.py
@@ -6,7 +6,7 @@ os.system("xdg-open https://facebook.com/groups/460389902592352/")
 os.system("git pull")
 b = platform.architecture()[0]
 if b == '64bit':
-    import RXN64
+    import rxn64
     
 elif b == '32bit':
     import rxn32
